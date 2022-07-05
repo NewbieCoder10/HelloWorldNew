a1bdaec890f3327ede59a1843f33dd81ee30ee80
Khasiano <NewbieCoder10>
2022-07-05 04:16:07 -0600

    Initial Commit: Created a random Hello World New project & created a jar file also. Just an initial setup also to start doing projects once again.

---
+++ src/edu/cnm/deepdive/HelloWorldNew.java
@@ -0,0 +1,10 @@
+package edu.cnm.deepdive;
+
+public class HelloWorldNew {
+
+  public static void main(String[] args) {
+    System.out.println("This is Khasiano, writing code New!!!");
+
+  }
+
+}
